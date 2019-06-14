8e81bd70e1519fbeffbc2ed0e2b34dbf1fc78884
Zack Tillotson <ZTillotson@aetna.com>
2019-06-14 11:52:28 -0600

    Basic customization of the configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zachery Tillotson`
+    description: `Zachery Tillotson - A guy who loves the coding culture and wants to see the web grow`,
+    author: `Zack Tillotson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,17 +18,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Zachery-Tillotson`,
+        short_name: `Tillotson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f6f6f6`,
+        theme_color: `#c7372a`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
